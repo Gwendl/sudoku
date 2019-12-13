faa1f19895cd569680209043c0c8b4db1eb8b848
gwendal lasson <gwendal.lasson@gmail.com>
2019-12-13 15:03:25 +0100

    init

--- app/src/main/java/com/myfu/sudoku/Grid.kt
+++ app/src/main/java/com/myfu/sudoku/Grid.kt
@@ -1,17 +1,22 @@
 package com.myfu.sudoku
 
-class Grid {
-    private var data = Array(6) { Array(5) { Token.BLANK  } }
-
-    fun initFromCsv(separator: String = ","): Grid
-    {
-        // implement
-        return this
-    }
+class Grid( private var data: Array<Array<Token>> = Array(6) { Array(5) { Token.BLANK  } }) {
 
     fun exportCsv(separator: String = ","): String
     {
         val lineToCsv = { line: Array<Token> -> line.joinToString(separator) { it.value.toString() } }
         return data.joinToString(separator, transform = lineToCsv)
+    }
+
+    fun clone(): Grid {
+        return Grid(data)
+    }
+
+    companion object {
+        fun initFromCsv(separator: String = ","): Grid
+        {
+            // implement
+            return Grid()
+        }
     }
 }
