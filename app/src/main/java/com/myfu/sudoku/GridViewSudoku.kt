cff61808ddba473965883cf8b8d11f23c1436164
myfou77 <zecchini.aymeric@gmail.com>
2019-12-13 16:06:05 +0100

    create gridviewsudoku class

---
+++ app/src/main/java/com/myfu/sudoku/GridViewSudoku.kt
@@ -0,0 +1,29 @@
+package com.myfu.sudoku
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+
+class GridViewSudoku : LinearLayout{
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0)
+            : super(context, attrs, defStyleAttr)
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int)
+            : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_grid, this, true)
+    }
+}
