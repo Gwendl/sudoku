6a2a22959b70d60747800a1a2254137aeb995163
gwendal lasson <gwendal.lasson@gmail.com>
2019-12-13 18:25:51 +0100

    csv parsing

--- app/src/main/java/com/myfu/sudoku/MainActivity.kt
+++ app/src/main/java/com/myfu/sudoku/MainActivity.kt
@@ -9,8 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val csv = Grid.fromCsv(getResources().getString(R.string.sudoku1)).exportCsv()
-        Log.i("csv", csv)
+        val csv = Grid.fromCsv(getResources().getString(R.string.sudoku1)).getSquares()
     }
 
 }
